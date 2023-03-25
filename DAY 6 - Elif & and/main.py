d7abb6c711682acb5cf2e8fdcd3afe53fc2fa341
Lucy <lucy.keedwell@hotmail.co.uk>
2023-03-25 21:00:41 +0000

    day 6 added

---
+++ DAY 6 - Elif & and/main.py
@@ -0,0 +1,11 @@
+print ("WELCOME TO CODEY LOGINS")
+username = input("USERNAME: ")
+password = input("PASSWORD: ")
+if username == "lucy" and password == "mog":
+  print ("Welcome to Codeytown Lucy!")
+elif username == "lewis" and password == "nova":
+  print ("Welcome to Codeytown Lewis!")
+elif username == "jess" and password == "tia":
+  print ("Welcome to Codeytown Jess!")
+else:
+  print ("ERROR, PLEASE TRY AGAIN")
