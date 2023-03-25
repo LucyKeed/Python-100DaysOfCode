b51e8e58d4b60851321a0e719044dee4012e1ca7
Lucy <lucy.keedwell@hotmail.co.uk>
2023-03-25 20:40:08 +0000

    added day 3

---
+++ DAY 3 - Concatenate/main.py
@@ -0,0 +1,11 @@
+food_type = input("Name a food dish: ")
+plant_type = input( "Now give us the name of a plant: ")
+cooking = input("And how would you like us to cook it?: ")
+burnt_desc = input("How would you describe burnt food? (asking for a friend): ")
+house_item = input("and lastly, give me the name of a household item: ")
+
+print()
+print("thanks for telling me that, i've cooked this dish for you, my love!")
+print()
+
+print("it is of course",cooking,food_type,"with",burnt_desc,plant_type,"with a side of chopped",house_item)
