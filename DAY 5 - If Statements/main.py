8493a80ef1951eeacc77f518cc22cf717718eb0e
Lucy <lucy.keedwell@hotmail.co.uk>
2023-03-25 20:57:17 +0000

    day 5 inputted

---
+++ DAY 5 - If Statements/main.py
@@ -0,0 +1,10 @@
+print ("*~*lets get to know you!*~*")
+starsign = input("what is your start sign? ")
+if starsign == "pisces":
+  print("you're so kind!")
+elif starsign == "leo":
+  print ("you're so courageous!")
+elif starsign == "virgo":
+  print ("you're so confident!")
+else: 
+  print ("fate is in your hands!")
