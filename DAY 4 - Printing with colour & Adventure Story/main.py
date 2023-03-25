ec0edb9cc1f14d4700f58c1f085b29a5cd201837
Lucy <lucy.keedwell@hotmail.co.uk>
2023-03-25 20:52:33 +0000

    created day 4

---
+++ DAY 4 - Printing with colour & Adventure Story/main.py
@@ -0,0 +1,12 @@
+pp = "\033[35m"
+w = "\033[0m"
+ 
+print ("Welcome to your",pp,"adventure",w,"simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!""")
+print()
+name = input ("Firstly, What is your name? ")
+worst_enemy = input("What is your worst enemys name? ")
+super_pow = input("What is your superpower? ")
+live = input ("Where do you live? ")
+fav_food = input("What is your favorite food?")
+print()
+print("Hello",name,"! Your ability to",super_pow,"will make sure you never have to look at",worst_enemy,"again. Go eat",fav_food, "as you walk down the streets of",live, "and use your power of",super_pow,"for good and not evil!")
